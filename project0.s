7d93ca3b59df515d5c5a454e28b58541bc85c7a0
SasheO <76496417+SasheO@users.noreply.github.com>
2022-09-27 21:37:22 -0400

    added lines to print from index 6 to end

--- project0.s
+++ project0.s
@@ -3,6 +3,10 @@ id: .asciiz "@03004056"
 
 .text
 main:
+li $v0, 4 # print string
+la $s0,id # address of string is in $s0 now
+la $a0, 6($s0) # so that the part of string s0 from index 6 gets printed
+syscall
 
 li $v0, 10 # end program
 syscall
