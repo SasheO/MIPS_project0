d5507aa5a82657f11e8daf1726fd6fee40261255
SasheO <76496417+SasheO@users.noreply.github.com>
2022-09-27 21:51:57 -0400

    included instructions to print from index 7 to end

--- project0.s
+++ project0.s
@@ -19,5 +19,12 @@ li $v0, 11 # print char
 li $a0, 10 # null character is ascii 10
 syscall # print new line
 
+li $v0, 4 # print string
+la $a0, 7($s0) # so that the part of string s0 from index 7 gets printed
+syscall
+
+
+
+
 li $v0, 10 # end program
 syscall
