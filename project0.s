3580e09977f101dde69d108d006373bd71681b00
SasheO <76496417+SasheO@users.noreply.github.com>
2022-09-27 21:54:35 -0400

    updated comments, included instructions to print new line

--- project0.s
+++ project0.s
@@ -7,12 +7,10 @@ li $v0, 4 # print string
 la $s0, id # address of string is in $s0 now
 la $a0, 6($s0) # so that the part of string s0 from index 6 gets printed
 syscall
-
 lb $t0, 6($s0) # temporarily store the value of index 6 in $t0
 sb $zero, 6($s0) # temporarily set index 6 to null character (00 in ascii, hence the $zero register)
 la $a0, 0($s0) # so that the string gets printed from beginning to null character
 syscall
-
 sb $t0, 6($s0) # revert the character in index 6 to previous value
 
 li $v0, 11 # print char
@@ -22,9 +20,15 @@ syscall # print new line
 li $v0, 4 # print string
 la $a0, 7($s0) # so that the part of string s0 from index 7 gets printed
 syscall
+lb $t0, 7($s0) # temporarily store the value of index 7 in $t0
+sb $zero, 7($s0) # temporarily set index 7 to null char
+la $a0, 0($s0) # so that the string gets printed from beginning to null char
+syscall
+sb $t0, 7($s0) # revert the character in index 7 to previous value
 
-
-
+li $v0, 11
+li $a0, 10
+syscall # print new line
 
 li $v0, 10 # end program
 syscall
