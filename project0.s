3d12218df9970b8d2292b80cccfbfb149ef959ae
SasheO <76496417+SasheO@users.noreply.github.com>
2022-09-27 21:43:04 -0400

    included instructions to print from beginning of string to before index 6

--- project0.s
+++ project0.s
@@ -4,8 +4,13 @@ id: .asciiz "@03004056"
 .text
 main:
 li $v0, 4 # print string
-la $s0,id # address of string is in $s0 now
+la $s0, id # address of string is in $s0 now
 la $a0, 6($s0) # so that the part of string s0 from index 6 gets printed
+syscall
+
+lb $t0, 6($s0) # temporarily store the value of index 6 in $t0
+sb $zero, 6($s0) # temporarily set index 6 to null character (00 in ascii, hence the $zero register)
+la $a0, 0($s0) # so that the string gets printed from beginning to null character
 syscall
 
 li $v0, 10 # end program
