72bb16c1280ac31a8d941ac6b155b280c3b4b775
SasheO <76496417+SasheO@users.noreply.github.com>
2022-09-27 22:10:56 -0400

    included instructions to print from index 3 to end

--- project0.s
+++ project0.s
@@ -83,5 +83,9 @@ li $a0, 10
 syscall # print new line
 
 
+li $v0, 4 # print string
+la $a0, 3($s0) # so that the part of string s0 from index 3 gets printed
+syscall
+
 li $v0, 10 # end program
 syscall
